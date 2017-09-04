d887d09f918a0711f93ca1d921bfba8a415170b1
Maciej Kocot <mkocot@op.pl>
2017-09-04 15:55:12 +0200

    updated different modes of plotting sim and reco

--- recoHits.cc
+++ recoHits.cc
@@ -6,42 +6,62 @@ gROOT->Reset();
 #include <math.h>
 using namespace std;
 
-// draw 2D reco histograms showing the events on six different RPs
-//
-// usage: the ROOT file to open should be in TFile file(...)
-// you can choose to plot only horizontal RPs (showVertical parameter)
-// then run it as a ROOT script
+// draw 2D histograms showing the events on six different RPs
 //
 // > root
 // > .x [path to the script]
 
+// ========= PARAMS =============
 
-// the reco plots in absolute coordinates can be transformed to the coordinates of the detector plane
-// translation (xt, yt) + rotation by 'a' degrees
-//
-// [ cos a	sin a	0	] [ 1	0	xt	] [ x ] [ x2 ]
-// [ sin a	-cos a	0	]*[ 0	1	yt	]*[ y ]=[ y2 ]
-// [ 0		0	1	] [ 0	0	1	] [ 1 ] [ 1  ]
-//
+// mode:
+// 	0 - draw all 6 RPs separately
+// 	1 - draw plots for near/far RPs separately
+// 	2 - draw plots for near/far horizontal RPs separately
+const int mode = 1;
 
-// PARAMS
-const bool transform = true; 
-const bool plotAll = false;
-TFile file("test_reco200000.root");
-// END PARAMS
+// filename:
+TFile file("test20000.root");
 
-// CONST
-const double xt[6] = { 0.0, 0.0, -20.0, -20.0, 0.0, 0.0 };
-const double yt[6] = { -18.0, 18.0, 0.0, 0.0, -18.0, 18.0 };
-const double degree[6] = { 135.0, -45.0, 45.0, 45.0, 135.0, -45.0 };
+// select the RPs to draw - leave (-1) to draw all of them
+const int arm = 0;
+const int station = 0;
+const int romanPot = -1;
+const int plane = -1; 
 
-string rpNames[6] = {"near top", "near bottom", "near horizontal", "far horizontal", "far top", "far bottom"};
-string rpGroupNames[2] = {"near", "far"};
-const int divX = plotAll ? 3 : 2;
-const int divY = plotAll ? 2 : 1;
+// ========= ENDPARAMS =========
+
+// =========== CONST ===========
+// first 6 RPs for right arm, then for the left
+// distances taken from RP_Dist_Beam_Cent.xml plus 11mm of th distance from the detector cut edge to its center
+const double xt[24] = { 0.0, 0.0, 17.480, 17.181, 0.0, 0.0, 0.0, 0.0, 50.0, 50.0, 0.0, 0.0,
+		0.0, 0.0, 17.480, 17.181, 0.0, 0.0, 0.0, 0.0, 17.480, 17.181, 0.0, 0.0 };
+
+const double yt[24] = { 20.129, -19.885, 0.0, 0.0, 19.043, -19.320, 50.0, -50.0, 0.0, 0.0, 50.0, -50.0,
+		20.052, -19.017, 0.0, 0.0, -18.553, 18.363, 50.0, -50.0, 0.0, 0.0, 50.0, -50.0 };
+
+// rotations - with the tilted right far pots in 210 station
+double degree[24] = { 135.0, -45.0, 45.0, 45.0, 135.0, -45.0, 135.0, -45.0, 45.0, 45.0, 135.0, -45.0,
+		135.0, -45.0, 45.0, 53.0, 143.0, -37.0, 135.0, -45.0, 45.0, 45.0, 135.0, -45.0 };
+
+for(int i=0; i<24; ++i) degree[i] = degree[i] * 3.14159 / 180;
+
+string rpNames[18] = {"near top", "near bottom", "near horizontal", "far horizontal", "far top", "far bottom",
+		"near hits", "far hits", "", "", "", "",
+		"near hits", "far hits", "", "", "", ""};
+
+const int divX = mode==0 ? 3 : 2;
+const int divY = mode==0 ? 2 : 1;
 const int RPCount = divX*divY;
-const int plotRange = 70;
-// END CONST
+const int minX = mode==0 ? -20 : (mode==1 ? -30 : -10);
+const int maxX = mode==0 ? -minX : (mode==1 ? 30 : 50);
+const int minY = mode==0 ? minX : (mode==1 ? -30 : -30);
+const int maxY = -minY;
+// =========== ENDCONST ========
+
+
+
+
+
 
 TTreeReader reader("Events", &file);
 TTreeReaderArray<edm::DetSet<TotemRPLocalTrack>> ps(reader, "TotemRPLocalTrackedmDetSetVector_totemRPLocalTrackFitter__TestFlatGun.obj._sets");
@@ -49,9 +69,7 @@ TTreeReaderArray<edm::DetSet<TotemRPLocalTrack>> ps(reader, "TotemRPLocalTracked
 TH2D** h = new TH2D*[RPCount]; 
 
 for(int i=0; i<RPCount; ++i) {
-	if(!transform && !plotAll) h[i] = new TH2D(rpGroupNames[i].c_str(), rpGroupNames[i].c_str(), 100, -plotRange, plotRange, 100, -plotRange, plotRange);
-	else if(transform && !plotAll) h[i] = new TH2D(rpNames[i+2].c_str(), rpNames[i+2].c_str(), 100, -plotRange, plotRange, 100, -plotRange, plotRange);
-	else h[i] = new TH2D(rpNames[i].c_str(), rpNames[i].c_str(), 100, -plotRange, plotRange, 100, -plotRange, plotRange);
+	h[i] = new TH2D(rpNames[6*mode+i].c_str(), rpNames[6*mode+i].c_str(), 100, minX, maxX, 100, minY, maxY);
 }
 
 while (reader.Next()) {
@@ -60,37 +78,36 @@ while (reader.Next()) {
 		int arId = (ps[i].detId()>> 24) & 0x1;
 		int stId = (ps[i].detId()>> 22) & 0x3;
 		int rpId = (ps[i].detId()>> 19) & 0x7;
-		int plId = (ps[i].detId()>> 15) & 0xF;		//always 0 in reco
+		int plId = (ps[i].detId()>> 15) & 0xF;
 
-		if(arId!=1 || stId!=0) continue;
-		if(transform && !plotAll && rpId!=2 && rpId!=3) continue;
+		if(mode==2 && rpId!=2 && rpId!=3) continue;
+		if((arm>=0 && arId!=arm) || (station>=0 && stId!=station) || (romanPot>=0 && rpId!=romanPot) || (plane>=0 && plId!=plane)) continue;
 
 		for (auto &psEl : ps[i]) {  
 			double xVal = psEl.getX0();
 			double yVal = psEl.getY0();
 
-			double d = degree[rpId] * 3.14159 / 180;
-			double xVal2 = transform ? ((xVal+xt[rpId])*cos(d) + (yVal+yt[rpId])*sin(d)) : xVal;
-			double yVal2 = transform ? ((xVal+xt[rpId])*sin(d) - (yVal+yt[rpId])*cos(d)) : yVal;
-
-			int hId = plotAll ? rpId : (transform ? rpId-2 : rpId/3);
-			h[hId]->Fill(xVal2, yVal2);
+			int graphId = mode==0 ? rpId : (mode==1 ? rpId/3 : rpId-2);
+			h[graphId]->Fill(xVal, yVal);
 		}
-
   	}
 }
 
+gStyle->SetPadGridX(1);
+gStyle->SetPadGridY(1);
 TCanvas *cc = new TCanvas("RPs", "RPs", 1800, 1800*divY/divX); 
 cc->Divide(divX, divY);
 
-int j=1;
+
 for(int i=0; i<RPCount; ++i) {
 	cc->cd(i+1);
+	cc->SetFixedAspectRatio(1.0);
 	h[i]->SetStats(false);
+	h[i]->GetXaxis()->SetTitle("x [mm]");
+	h[i]->GetYaxis()->SetTitle("y [mm]");
 	h[i]->Draw("COLZ");
 }
 
-
-
 }
+
 
--- rps2.cc
+++ rps2.cc
@@ -20,11 +20,11 @@ using namespace std;
 const int mode = 1;
 
 // filename:
-TFile file("test.root");
+TFile file("test20000.root");
 
 // select the RPs to draw - leave (-1) to draw all of them
-const int arm = 0;
-// in this script we only use station 0 - the others were not used during our run
+const int arm = 1;
+const int station = 0;
 const int romanPot = -1;
 const int plane = 1; 
 
@@ -33,15 +33,29 @@ const int plane = 1;
 // =========== CONST ===========
 // first 6 RPs for right arm, then for the left
 // distances taken from RP_Dist_Beam_Cent.xml plus 11mm of th distance from the detector cut edge to its center
-const double xt[12] = { 0.0, 0.0, 17.605, 16.319, 0.0, 0.0, 0.0, 0.0, 17.480, 17.181, 0.0, 0.0 };
-const double yt[12] = { 20.052, -19.017, 0.0, 0.0, 18.553, -18.363, 20.129, -19.885, 0.0, 0.0, 19.043, -19.320 };
-double degree[12] = { 135.0, -45.0, 45.0, 45.0, 135.0, -45.0, -45.0, 45.0, -45.0, -45.0, -135.0, 45.0 };	// todo the tilted ones
-for(int i=0; i<12; ++i) degree[i] = degree[i] * 3.14159 / 180;
-string rpNames0[6] = {"near top", "near bottom", "near horizontal", "far horizontal", "far top", "far bottom"};
-string rpNames1[6] = {"", "", "", "", "", ""};	//todo
+const double xt[24] = { 0.0, 0.0, 17.480, 17.181, 0.0, 0.0, 0.0, 0.0, 50.0, 50.0, 0.0, 0.0,
+		0.0, 0.0, 17.480, 17.181, 0.0, 0.0, 0.0, 0.0, 17.480, 17.181, 0.0, 0.0 };
+
+const double yt[24] = { 20.129, -19.885, 0.0, 0.0, 19.043, -19.320, 50.0, -50.0, 0.0, 0.0, 50.0, -50.0,
+		20.052, -19.017, 0.0, 0.0, -18.553, 18.363, 50.0, -50.0, 0.0, 0.0, 50.0, -50.0 };
+
+// rotations - with the tilted right far pots in 210 station
+double degree[24] = { 135.0, -45.0, 45.0, 45.0, 135.0, -45.0, 135.0, -45.0, 45.0, 45.0, 135.0, -45.0,
+		135.0, -45.0, 45.0, 53.0, 143.0, -37.0, 135.0, -45.0, 45.0, 45.0, 135.0, -45.0 };
+
+for(int i=0; i<24; ++i) degree[i] = degree[i] * 3.14159 / 180;
+
+string rpNames[18] = {"near top", "near bottom", "near horizontal", "far horizontal", "far top", "far bottom",
+		"near hits", "far hits", "", "", "", "",
+		"near hits", "far hits", "", "", "", ""};
+
 const int divX = mode==0 ? 3 : 2;
 const int divY = mode==0 ? 2 : 1;
 const int RPCount = divX*divY;
+const int minX = mode==0 ? -20 : (mode==1 ? -50 : -10);
+const int maxX = mode==0 ? -minX : (mode==1 ? 50 : 50);
+const int minY = mode==0 ? minX : (mode==1 ? -50 : -30);
+const int maxY = -minY;
 // =========== ENDCONST ========
 
 
@@ -55,7 +69,7 @@ TTreeReaderArray<PSimHit> ps(reader, "PSimHits_g4SimHits_TotemHitsRP_TestFlatGun
 TH2D** h = new TH2D*[RPCount]; 
 
 for(int i=0; i<RPCount; ++i) {
-	h[i] = new TH2D(rpNames0[i].c_str(), rpNames0[i].c_str(), 100, -50, 50, 100, -50, 50);
+	h[i] = new TH2D(rpNames[6*mode+i].c_str(), rpNames[6*mode+i].c_str(), 100, minX, maxX, 100, minY, maxY);
 }
 
 while (reader.Next()) {
@@ -67,17 +81,24 @@ while (reader.Next()) {
 		int plId = (ps[i].detUnitId()>> 15) & 0xF;
 
 		if(mode==2 && rpId!=2 && rpId!=3) continue;
-		if((arm>=0 && arId!=arm) || (stId!=0) || (romanPot>=0 && rpId!=romanPot) || (plane>=0 && plId!=plane)) continue;
+		if((arm>=0 && arId!=arm) || (station>=0 && stId!=station) || (romanPot>=0 && rpId!=romanPot) || (plane>=0 && plId!=plane)) continue;
 
 		double xVal = ps[i].entryPoint().x();
 		double yVal = ps[i].entryPoint().y();
 	
-		if(mode==1) {
-			int rpPosId = 6*arId+rpId;
+		if(mode==1 || mode==2) {
 			double xVal2 = xVal;
 			double yVal2 = yVal;
-			xVal = xVal2*cos(degree[rpPosId]) + yVal2*sin(degree[rpPosId]) + xt[rpPosId];
-			yVal = xVal2*sin(degree[rpPosId]) - yVal2*cos(degree[rpPosId]) + yt[rpPosId];
+			int rpPosId = 12*arId + 6*(stId==0 ? 0 : 1) + rpId;
+
+			if(arId==1 && stId==0 && rpId>2) {	//rotated pots
+				double eightDeg = 8 * 3.14159 / 180;
+				xVal = xVal2*cos(degree[rpPosId]) + yVal2*sin(degree[rpPosId]) + xt[rpPosId]*cos(eightDeg) + yt[rpPosId]*sin(eightDeg);
+				yVal = xVal2*sin(degree[rpPosId]) - yVal2*cos(degree[rpPosId]) + xt[rpPosId]*sin(eightDeg) - yt[rpPosId]*cos(eightDeg);
+			} else {
+				xVal = xVal2*cos(degree[rpPosId]) + yVal2*sin(degree[rpPosId]) + xt[rpPosId];
+				yVal = xVal2*sin(degree[rpPosId]) - yVal2*cos(degree[rpPosId]) + yt[rpPosId];
+			}
 		}
 
 		int graphId = mode==0 ? rpId : (mode==1 ? rpId/3 : rpId-2);
@@ -85,16 +106,20 @@ while (reader.Next()) {
   	}
 }
 
+gStyle->SetPadGridX(1);
+gStyle->SetPadGridY(1);
 TCanvas *cc = new TCanvas("RPs", "RPs", 1800, 1800*divY/divX); 
 cc->Divide(divX, divY);
 
+
 for(int i=0; i<RPCount; ++i) {
 	cc->cd(i+1);
+	cc->SetFixedAspectRatio(1.0);
 	h[i]->SetStats(false);
+	h[i]->GetXaxis()->SetTitle("x [mm]");
+	h[i]->GetYaxis()->SetTitle("y [mm]");
 	h[i]->Draw("COLZ");
 }
-
-
 
 }
 
